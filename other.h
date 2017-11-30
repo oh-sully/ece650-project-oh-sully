fd77980470d29fedf456e36e3b520ee0e0e4bc58
Brendan O'Sullivan <brendan.alex.osullivan@gmail.com>
2017-11-30 16:20:38 -0500

    add matrix to other.h

--- other.h
+++ other.h
@@ -15,5 +15,36 @@
 // defines vec
 #include "minisat/mtl/Vec.h"
 
+class Matrix {
+private:
+    int rows = 0, cols = 0, edgenum = 0;
+public:
+    std::vector<bool> matrix;
+
+    Matrix() {
+        rows = 0;
+        cols = 0;
+        matrix.resize(0, 0);
+    }
+
+    Matrix(int n, int k, bool str_value) {
+        rows = n;
+        cols = k;
+        matrix.resize(rows * cols, str_value);
+    }
+    
+    void edit(int n, int k, bool value);
+    
+    int value(int n, int k);
+    //returns the number of edges with vertex 'v'
+    int num_of_edges(int v);
+    //returns the total number of edges
+    int num_of_edges();
+    //sets to 0 all edges that contain vertex 'v'
+    void clear_edges(int v);
+   //prints the adjacency matrix
+    void print();
+};
+
 //thread for io
 void *io_thread(void *args);

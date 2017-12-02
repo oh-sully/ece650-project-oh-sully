cfc21dab7d8d0489c39abadd75e2ffd316a8c177
Brendan O'Sullivan <brendan.alex.osullivan@gmail.com>
2017-12-02 15:12:51 -0500

    add V 14 only

--- project.cpp
+++ project.cpp
@@ -606,7 +606,7 @@ void *io_thread(void *args){
     //X.push_back(11);
     //X.push_back(13);
     //X.push_back(15);
-    X.push_back(17);
+    X.push_back(14);
     std::ofstream datafile ("../datafile.dat");//to remove when ready to submit
     datafile << "#X      SATtime        SATsd        VC1time        VC1sd        VC2time       VC2sd     r1  rsd1  r2  rsd2" << std::endl;//remove when ready to submit
     datafile.close();//remove when ready to submit
